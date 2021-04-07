1f00b76f3d626c773fef4d87c783d6a26c685cf0
Sydney Bonner <sebonner1@crimson.ua.edu>
2021-04-07 13:12:51 -0500

    working on the login to dashboard link

--- Client/resources/scripts/login.js
+++ Client/resources/scripts/login.js
@@ -2,14 +2,17 @@ const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
+// the below syntax from research
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "user" && password === "web_dev") {
+    // The manager login is below
+    if (username === "TTCManager" && password === "TitleTown") {
         alert("You have successfully logged in.");
         location.reload();
+        let html = "<a href="../ManagerDasboard.html"></a>";
     } else {
         loginErrorMsg.style.opacity = 1;
     }
