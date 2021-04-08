cda54fd6a8bc0123157dffb70e558e45b7079b81
Sydney Bonner <sebonner1@crimson.ua.edu>
2021-04-08 16:46:42 -0500

    payment method form

--- Client/resources/scripts/receipt.js
+++ Client/resources/scripts/receipt.js
@@ -5,3 +5,58 @@ checkoutbutton.addEventListener("click", (e) => {
     alert("You are being directed to the calendar.");
     window.location.href= "/Users/williambonner/Source/repos/TitleTownCatering/Client/resources/Calendar.html";
 })
+
+var owner = $('#owner'),
+    cardNumber = $('#cardNumber'),
+    cardNumberField = $('#card-number-field'),
+    CVV = $("#cvv"),
+    mastercard = $("#mastercard"),
+    confirmButton = $('#confirm-purchase'),
+    visa = $("#visa"),
+    amex = $("#amex");
+
+cardNumber.payform('formatCardNumber');
+CVV.payform('formatCardCVC');
+
+cardNumber.keyup(function() {
+    amex.removeClass('transparent');
+    visa.removeClass('transparent');
+    mastercard.removeClass('transparent');
+
+    if ($.payform.validateCardNumber(cardNumber.val()) == false) {
+        cardNumberField.removeClass('has-success');
+        cardNumberField.addClass('has-error');
+    } else {
+        cardNumberField.removeClass('has-error');
+        cardNumberField.addClass('has-success');
+    }
+
+    if ($.payform.parseCardType(cardNumber.val()) == 'visa') {
+        mastercard.addClass('transparent');
+        amex.addClass('transparent');
+    } else if ($.payform.parseCardType(cardNumber.val()) == 'amex') {
+        mastercard.addClass('transparent');
+        visa.addClass('transparent');
+    } else if ($.payform.parseCardType(cardNumber.val()) == 'mastercard') {
+        amex.addClass('transparent');
+        visa.addClass('transparent');
+    }
+});
+
+confirmButton.click(function(e) {
+    e.preventDefault();
+
+    var isCardValid = $.payform.validateCardNumber(cardNumber.val());
+    var isCvvValid = $.payform.validateCardCVC(CVV.val());
+
+    if(owner.val().length < 5){
+        alert("Wrong owner name");
+    } else if (!isCardValid) {
+        alert("Wrong card number");
+    } else if (!isCvvValid) {
+        alert("Wrong CVV");
+    } else {
+        // Everything is correct. Add your form submission code here.
+        alert("Everything is correct");
+    }
+});
