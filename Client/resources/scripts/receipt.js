59fb4d53946783cd1e6a290dd8f9d58e3da8941d
Sydney Bonner <sebonner1@crimson.ua.edu>
2021-04-07 21:35:14 -0500

    calendar feature

--- Client/resources/scripts/receipt.js
+++ Client/resources/scripts/receipt.js
@@ -2,6 +2,6 @@ const checkoutbutton = document.getElementById("checkoutbutton");
 
 // the below syntax from research
 checkoutbutton.addEventListener("click", (e) => {
-    alert("You are being directed to checkout.");
-    window.location.href= "/Users/williambonner/Source/repos/TitleTownCatering/Client/resources/Receipt.html";
+    alert("You are being directed to the calendar.");
+    window.location.href= "/Users/williambonner/Source/repos/TitleTownCatering/Client/resources/Calendar.html";
 })
