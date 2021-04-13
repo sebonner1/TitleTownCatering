c652bc1f5e93244095cf9e25fac2106ba13cf828
Katherine Pearson <katherine@Katherines-MacBook-Pro-2.local>
2021-04-12 19:21:53 -0500

    oof

--- Cartfunctions/removeCart/delCart.cs
+++ Cartfunctions/removeCart/delCart.cs
@@ -4,6 +4,17 @@ namespace TitleTownCatering.Cartfunctions.removeCart
 {
     public class delCart : iDelCart
     {
-        
+        public void DeleteCartItem(int value)
+        {
+            string cs = @"placeholder";
+            using var con = new SQLiteConnection(cs);
+            con.Open(); 
+
+            string stm = @"DELETE FROM posts WHERE postid =@id";
+            using var cmd = new SQLiteCommand(stm, con);
+            cmd.Parameters.AddWithValue("@id",value);
+            cmd.Prepare();
+            cmd.ExecuteNonQuery();
+        }
     }
 }
